e58549379a1f02a075019999b8ecbd832896b325
omarkhaledafifi <omarafifi606@gmail.com>
2025-09-17 14:15:22 +0300

    edit productResponse

--- Shared/DTOs/Products/ProductResponse.cs
+++ Shared/DTOs/Products/ProductResponse.cs
@@ -13,7 +13,9 @@ namespace Shared.DTOs.Products
         public string Description { get; set; }
         public string PictureUrl { get; set; }
         public Decimal Price { get; set; }
+        public int BrandId { get; set; }
         public string BrandName { get; set; }
+        public int TypeId { get; set; }
         public string TypeName { get; set; }
 
     }
